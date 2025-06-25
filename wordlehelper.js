44006ce7bdf7f138ad62c3bc97366e27a6360592
Jake Feasel <jfeasel@gmail.com>
2025-06-25 11:53:02 -0700

    Handle case when there is a collision between a non-matching letter and an inexactly-matching letter

--- wordlehelper.js
+++ wordlehelper.js
@@ -36,11 +36,21 @@ function sortWords (currentWordList) {
 function filterWords(validWords, matchPattern, notFound) {
     let filterRegex;
 
-    inexactMatches = matchPattern
+    let inexactMatches = matchPattern
         .split("")
         .filter(c => c.match(/[A-Z]/))
         .map(c => c.toLowerCase())
         .join("");
+
+    // Deal with the case where we have at least two instances of the same letter
+    // In at least one instance, it is inexact, and the others it is not a match at all
+    notFound = notFound.split("").filter(letter =>
+        // It's best to only consider the inexact matches in these cases, and forget
+        // about the notFound cases. Yes, this means there will be some rare instances
+        // when we suggest a word that couldn't fit due to a previously-excluded letter
+        // position, but it's better than accidentally filtering out a valid option.
+        (inexactMatches.indexOf(letter) === -1)
+    ).join("");
 
     matchPattern = matchPattern.replaceAll(/([A-Z])/g, (result) => `[^${result.toLowerCase()}X]`);
 
